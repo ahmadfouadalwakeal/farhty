dd246cce6e802fa99d56cf50068b42466acae07e
ahmadfouadalwakeal <ahmedfouad2592001@gmail.com>
2024-12-29 11:50:29 +0200

    first commit

---
+++ lib/app/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const MyHomePage(),
+    );
+  }
+}
+
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key,});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
