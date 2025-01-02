2215410846bfd886d5df7878704cd516da86c1d9
ahmadfouadalwakeal <ahmedfouad2592001@gmail.com>
2025-01-02 12:56:56 +0200

    -add  select image picker from camera & gallery

--- lib/features/auth/presentation/widgets/profile_image.dart
+++ lib/features/auth/presentation/widgets/profile_image.dart
@@ -1,9 +1,57 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../../../core/theming/colors.dart';
 
-class ProfileImage extends StatelessWidget {
+class ProfileImage extends StatefulWidget {
   const ProfileImage({super.key});
+
+  @override
+  _ProfileImageState createState() => _ProfileImageState();
+}
+
+class _ProfileImageState extends State<ProfileImage> {
+  File? _image;
+
+  Future<void> _pickImage(ImageSource source) async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: source);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  void _showImageSourceDialog(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ListTile(
+            leading: const Icon(Icons.camera_alt),
+            title: const Text('الكاميرا'),
+            onTap: () {
+              Navigator.of(ctx).pop();
+              _pickImage(ImageSource.camera);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.photo_library),
+            title: const Text('معرض الصور'),
+            onTap: () {
+              Navigator.of(ctx).pop();
+              _pickImage(ImageSource.gallery);
+            },
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,17 +60,20 @@ class ProfileImage extends StatelessWidget {
         CircleAvatar(
           radius: 50,
           backgroundColor: Colors.grey.shade300,
-          child: const Icon(
-            Icons.person,
-            size: 70,
-            color: Colors.grey,
-          ),
+          backgroundImage: _image != null ? FileImage(_image!) : null,
+          child: _image == null
+              ? const Icon(
+                  Icons.person,
+                  size: 70,
+                  color: Colors.grey,
+                )
+              : null,
         ),
         Positioned(
           bottom: 0,
           right: 0,
           child: GestureDetector(
-            onTap: () {},
+            onTap: () => _showImageSourceDialog(context),
             child: Container(
               decoration: BoxDecoration(
                 color: ColorsManager.mainDarkPurple,
